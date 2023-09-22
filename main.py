96e96b47b88ba17158a8add34dc1f7c6eeb383f2
RD_Laptop <go_gamer8@hotmail.com>
2023-09-22 07:32:34 +0400

    1. Created an error message when an invalid link is used 2. Made InstagramDownload check if the link is a video link first then if it is a photo link (for efficiency purposes) 3. Added a "/" after "youtu.be" 4. Moved all functions to the top of the code 5. Fixed a bug : the variable "youtubeAudioAbsloutePath" used to be deleted before os.remove can delete the file 6. instead of the program finding the parent folder of the video file, it now uses the variable "pathToExportYoutubeVideo" 7. The final video is renamed to it's YouTube title 8. Made the opening of a file in a location inside Windows explorer a function

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ guiCheckbox = False
 
 randomNumber = str(random.randint(10000000,99999999))
 #Defining functions
+
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    guiExportPath = filedialog.askdirectory()
+    print(guiExportPath)
+    guiOutputLocationEntry.delete(0, END)
+    guiOutputLocationEntry.insert(0, guiExportPath)
+
+
 def downloadYoutubeVideo(url: str, exportPath : str):
     guiProgressBar.set(0.05)
     guiProgressBarText.configure(text ="Downloading YouTube Video")
@@ -42,10 +53,95 @@ def extractAudio(pathToVideo : str):
     audio.write_audiofile((str(pathlib.Path(pathToVideo).absolute().parent))+'\\'+randomNumber+".mp3", logger = None)
     audio.close()
 
+def openFileLocation(yesOrNo : bool, fileLocation):
+    if yesOrNo == True:
+        subprocess.run(f'explorer /select,"{fileLocation}"')
 
 
+def YouTubeDownload(theYoutubeVideoLink : str, pathToExportYoutubeVideo, guiCheckbox1):
 
 
+    #Getting a full path to the export location
+    pathToExportYoutubeVideo = str(pathlib.Path(pathToExportYoutubeVideo).absolute())
+
+    #Downloading the YouTube Video and Audio
+    guiProgressBar.set(0.05)
+    guiProgressBarText.configure(text ="Downloading YouTube Video", text_color= 'white')
+    YouTube(theYoutubeVideoLink).streams.filter(adaptive=True, file_extension= 'mp4').order_by('resolution').last().download(pathToExportYoutubeVideo, filename=randomNumber + '.mp4')
+    guiProgressBar.set(0.1)
+    guiProgressBarText.configure(text ="Downloading YouTube Audio")
+    YouTube(theYoutubeVideoLink).streams.filter(file_extension= 'mp4').last().download(pathToExportYoutubeVideo, filename=randomNumber + '_audio.mp4')
+
+    #Defining youtubeVideo for the usage of youtubeVideo.title and for refrencing paths
+    youtubeVideo = YouTube(theYoutubeVideoLink)
+    youtubeVideoAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'.mp4')
+    youtubeAudioAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'_audio.mp4')
+
+
+    #extractAudio(youtubeAudioAbsloutePath)
+    #Extracting the mp3 file from the mp4 audio file (yes, an mp4 audio file)
+    guiProgressBar.set(0.15)
+    guiProgressBarText.configure(text ="Converting Youtube Audio to a Usable Type", text_color= 'black')
+    audio = AudioFileClip(youtubeAudioAbsloutePath)
+    audio.write_audiofile(pathToExportYoutubeVideo + '\\' +randomNumber+ ".mp3" , logger = None)
+    audio.close()
+
+    os.remove(youtubeAudioAbsloutePath)
+    youtubeAudioAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'.mp3')
+
+    guiProgressBar.set(0.70)
+    videoWithoutSound = VideoFileClip(youtubeVideoAbsloutePath)
+    videoWithSound = videoWithoutSound.set_audio(AudioFileClip(youtubeAudioAbsloutePath))
+
+    #Getting the bitrate of the originial video
+    bit_rate =str(int((os.path.getsize(youtubeVideoAbsloutePath)/1000000)/((videoWithoutSound.duration/60)*0.0075))*1000)
+    #bit_rate = str(bit_rate*1000)
+
+    #Creating the final video
+    guiProgressBarText.configure(text ="Getting the Final Video")
+    videoWithSound.write_videofile(pathToExportYoutubeVideo + '\\' + youtubeVideo.title + '.mp4', bitrate= bit_rate, verbose= False, logger = None)
+    guiProgressBar.set(1)
+    guiProgressBarText.configure(text = "Finished!")
+
+    #deleting variables so at the end the program would be able to delete files that are not needed anymore
+    videoWithoutSound.close()
+    videoWithSound.close()
+    os.remove(youtubeVideoAbsloutePath)
+    os.remove(youtubeAudioAbsloutePath)
+
+    #path = (fr'{pathToExportYoutubeVideo}\{youtubeVideo.title}.mp4')
+
+    #Open the final video file in windows explorer
+    openFileLocation(guiCheckbox1, (f'{pathToExportYoutubeVideo}/{youtubeVideo.title}.mp4'))
+    #if guiCheckbox1 == True:
+    #    subprocess.run(f'explorer /select,"{pathToExportYoutubeVideo}/{youtubeVideo.title}.mp4"')
+
+def InstagramDownload(instaLink, outputLocation, guiCheckbox1):
+    cl = Client()
+    outputLocation = (pathlib.Path(outputLocation).absolute())
+    instaInfo = str(cl.media_info(cl.media_pk_from_url(instaLink)))
+
+    if 'media_type=2' in instaInfo:
+        guiProgressBarText.configure(text="Downloading Instagram Video", text_color= 'black')
+        cl.video_download(cl.media_pk_from_url(instaLink), outputLocation)
+
+
+    elif 'media_type=1' in instaInfo:
+        guiProgressBarText.configure(text="Downloading Instagram Photo", text_color= 'black')
+        cl.photo_download(cl.media_pk_from_url(instaLink), outputLocation)
+    #if guiCheckbox1 == True:
+    #    subprocess.run(f'explorer /select,"{outputLocation}/{youtubeVideo.title}.mp4"')
+
+
+def guiStart():
+    if 'youtube.com/' in (guiLinkEntry.get()) or  'youtu.be/' in (guiLinkEntry.get()):
+        t = Thread(target=YouTubeDownload, args=(str(guiLinkEntry.get()), str(guiOutputLocationEntry.get()), bool(checkBox.get())), daemon=True)
+        t.start()
+    elif 'instagram.com/' in (guiLinkEntry.get()):
+        t = Thread(target=InstagramDownload, args=(str(guiLinkEntry.get()), str(guiOutputLocationEntry.get()), bool(checkBox.get())), daemon=True)
+        t.start()
+    else:
+        guiProgressBarText.configure(text="Invalid Link", text_color= 'red')
 
 #GUI
 
@@ -53,7 +149,7 @@ customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('dark-blue')
 
 root = customtkinter.CTk()
-root.geometry('1000x500')
+root.geometry('700x450')
 root.resizable(0,0)
 root.title('All Social Media Downloader')
 #root.after(201, lambda :root.iconbitmap("./youtubeWhiteLogo.ico"))
@@ -72,99 +168,11 @@ guiOutputLocationEntry.pack(pady=12, padx=10)
 checkBox=customtkinter.CTkCheckBox(master = frame, text="Open File Location When Done")
 checkBox.pack(pady=5, padx=10)
 
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    guiExportPath = filedialog.askdirectory()
-    print(guiExportPath)
-    guiOutputLocationEntry.delete(0, END)
-    guiOutputLocationEntry.insert(0, guiExportPath)
+
 
 guiOutputLocationButton = customtkinter.CTkButton(master=frame, height= 30, text ="Select Output Location", command=browse_button)
 guiOutputLocationButton.pack(pady=5, padx=0)
 
-def InstagramDownload(instaLink, outputLocation, guiCheckbox1):
-    cl = Client()
-    # cl.login(USERNAME, PASSWORD)
-
-    instaLink
-    instaInfo = str(cl.media_info(cl.media_pk_from_url(instaLink)))
-    if 'media_type=1' in instaInfo:
-        cl.photo_download(cl.media_pk_from_url(instaLink))
-
-    elif 'media_type=2' in instaInfo:
-        cl.video_download(cl.media_pk_from_url(instaLink))
-
-
-    #if guiCheckbox1 == True:
-    #    subprocess.run(f'explorer /select,"{outputLocation}/{youtubeVideo.title}.mp4"')
-
-def YouTubeDownload(theYoutubeVideoLink : str, pathToExportYoutubeVideo, guiCheckbox1):
-
-
-    #Defining the main variables
-    pathToExportYoutubeVideo = str(pathlib.Path(pathToExportYoutubeVideo).absolute())
-
-    #Downloading the YouTube Video
-    guiProgressBar.set(0.05)
-    guiProgressBarText.configure(text ="Downloading YouTube Video")
-    YouTube(theYoutubeVideoLink).streams.filter(adaptive=True, file_extension= 'mp4').order_by('resolution').last().download(pathToExportYoutubeVideo, filename=randomNumber + '.mp4')
-    guiProgressBar.set(0.1)
-    guiProgressBarText.configure(text ="Downloading YouTube Audio")
-    YouTube(theYoutubeVideoLink).streams.filter(file_extension= 'mp4').last().download(pathToExportYoutubeVideo, filename=randomNumber + '_audio.mp4')
-
-    #Defining youtubeVideo for the usage of youtubeVideo.title and for refrencing paths
-    youtubeVideo = YouTube(theYoutubeVideoLink)
-    youtubeVideoAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'.mp4')
-    youtubeAudioAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'_audio.mp4')
-
-
-    extractAudio(youtubeAudioAbsloutePath)
-    #Extracting the mp3 file from the mp4 audio file (yes, an mp4 audio file)
-    guiProgressBar.set(0.15)
-    guiProgressBarText.configure(text ="Converting Youtube Audio to a Usable Type")
-    audio = AudioFileClip(youtubeAudioAbsloutePath)
-    audio.write_audiofile((str(pathlib.Path(youtubeVideoAbsloutePath).absolute().parent))+'\\'+randomNumber+".mp3", logger = None)
-    audio.close()
-
-    os.remove(youtubeAudioAbsloutePath)
-    youtubeAudioAbsloutePath = (pathToExportYoutubeVideo+'\\'+randomNumber+'.mp3')
-
-    guiProgressBar.set(0.70)
-    videoWithoutSound = VideoFileClip(youtubeVideoAbsloutePath)
-    videoWithSound = videoWithoutSound.set_audio(AudioFileClip(youtubeAudioAbsloutePath))
-
-    #Getting the bitrate of the originial video
-    bit_rate =int((os.path.getsize(youtubeVideoAbsloutePath)/1000000)/((videoWithoutSound.duration/60)*0.0075))
-    bit_rate = str(bit_rate*1000)
-
-    #Creating the final video
-    guiProgressBarText.configure(text ="Getting the Final Video")
-    videoWithSound.write_videofile(pathToExportYoutubeVideo + '\\' + youtubeVideo.title + '.mp4', bitrate= bit_rate, verbose= False, logger = None)
-    guiProgressBar.set(1)
-    guiProgressBarText.configure(text = "Finished!")
-
-    #deleting variables so at the end the program would be able to delete files that are not needed anymore
-    videoWithoutSound.close()
-    videoWithSound.close()
-    os.remove(youtubeVideoAbsloutePath)
-    os.remove(youtubeAudioAbsloutePath)
-
-
-    #Open the final video file in windows explorer
-    if guiCheckbox1 == True:
-        subprocess.run(f'explorer /select,"{pathToExportYoutubeVideo}\{youtubeVideo.title}.mp4"')
-
-
-
-
-def guiStart():
-    if 'youtube.com/' in (guiLinkEntry.get()) or  'youtu.be' in (guiLinkEntry.get()):
-        t = Thread(target=YouTubeDownload, args=(str(guiLinkEntry.get()), str(guiOutputLocationEntry.get()), bool(checkBox.get())), daemon=True)
-        t.start()
-    elif 'instagram.com/' in (guiLinkEntry.get()):
-        t = Thread(target=InstagramDownload, args=(str(guiLinkEntry.get()), str(guiOutputLocationEntry.get()), bool(checkBox.get())), daemon=True)
-        t.start()
 
 guiStartButton = customtkinter.CTkButton(master=frame, height= 50, text="Start", command= guiStart)
 guiStartButton.pack(pady=20, padx=10)
